cc16a75d7139f4024f50cf93696e8510667bda01
Malick <malick.hamidou.boureima@gmail.com>
2023-03-15 21:47:19 -0400

    Secure password

--- email_send.py
+++ email_send.py
@@ -7,8 +7,7 @@ def send_email(message):
     port = 465
 
     username = "malicksender@gmail.com"
-    # password = os.getenv("PASSWORD")
-    password = "vgvmmjfdhubibxdv"
+    password = os.getenv("PASSWORD")
 
     receiver = "malicksender@gmail.com"
     context = ssl.create_default_context()
